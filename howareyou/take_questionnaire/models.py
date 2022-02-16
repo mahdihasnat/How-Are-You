22fc2165861ed8a002ec56ba175fb7637d2c35b3
vodro <vdrkowshic@gmail.com>
2022-02-16 22:37:08 +0600

    Question model class working.

--- howareyou/take_questionnaire/models.py
+++ howareyou/take_questionnaire/models.py
@@ -4,13 +4,23 @@ from django.db import models
 
 
 ID_LENGTH = 15
+FIELD_LENGTH = 20
+UNSPECIFIED = 'unspecified'
 
-'''Test'''
+
+# -question_id : int
+# -type : string
+# -added_by : Psychiatrist
+# -created_at : datetime
+# -question_text : string
+# -options : list()
+# - status : char
 
 
 class Question(models.Model):
     id = models.CharField("unique id to every question",
                           max_length=ID_LENGTH, primary_key=True)
+    type = models.CharField(max_length=FIELD_LENGTH, default=UNSPECIFIED)
 
     def str(self):
         return self.id
