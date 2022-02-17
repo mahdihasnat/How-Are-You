781801a58d03b0aa83f13488081b6b12d7f06636
vodro <vdrkowshic@gmail.com>
2022-02-17 14:11:14 +0600

    question text added

--- howareyou/take_questionnaire/models.py
+++ howareyou/take_questionnaire/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy
 
 # Create your models here.
-
+# from howareyou.psychiatrists.models import Psychiatrist
 
 ID_LENGTH = 15
 FIELD_LENGTH = 20
+TEXT_FIELD = 1000
 UNSPECIFIED = 'unspecified'
 
 
@@ -18,9 +21,27 @@ UNSPECIFIED = 'unspecified'
 
 
 class Question(models.Model):
-    id = models.CharField("unique id to every question",
-                          max_length=ID_LENGTH, primary_key=True)
-    type = models.CharField(max_length=FIELD_LENGTH, default=UNSPECIFIED)
+    # do we need id ?
+    # id = models.CharField("unique id to every question",
+    #                       max_length=ID_LENGTH, primary_key=True)
 
-    def str(self):
-        return self.id
+    # added_by = models.ForeignKey(Psychiatrist)
+    # should we not have a string type question
+    class QuestionType(models.TextChoices):
+        RADIO = 'RD', gettext_lazy('Radio')
+        SLIDER = 'SL', gettext_lazy('Number Slider')
+        MUL_CHOICE = 'CH', gettext_lazy('Multiple Choice')
+
+    type = models.CharField(max_length=2, choices=QuestionType.choices, default=QuestionType.RADIO)
+
+    created_at = models.DateTimeField(default=timezone.now)
+    # added by me
+    changed_at = models.DateTimeField(default=timezone.now)
+
+    question_text = models.CharField(max_length=TEXT_FIELD)
+
+    class QuestionStatus(models.TextChoices):
+        pass
+
+    # def str(self):
+    #     return self
