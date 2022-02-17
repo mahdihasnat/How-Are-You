6b46338537ee826186b03bac7139a799afedffb0
vodro <vdrkowshic@gmail.com>
2022-02-17 23:01:29 +0600

    templates connected with view

--- howareyou/take_questionnaire/views.py
+++ howareyou/take_questionnaire/views.py
@@ -1,9 +1,21 @@
-from django.http import HttpResponse
-
-
 # Create your views here.
+from django.shortcuts import render
 
-def take_questionnaire(request):
-    response = HttpResponse("Now please render")
-    return response
-    # return render(request, "hello")
+
+def render_a_test(request):
+    # test = ["OCD", "HISTERIA"]
+    # dict = {"test" : test}
+    # now = datetime.datetime.now()
+    # html = "<html><body>It is now %s.</body></html>" % now
+    # return HttpResponse(html)
+    return render(request, 'take_questionnaire/test-questions.html')
+
+
+# This is actually user homepage :3
+# def render_test_list(request):
+#
+#     return HttpResponse('return all  tests!')
+
+
+def render_submitted(request):
+    return render(request, 'take_questionnaire/response-recorded.html')
