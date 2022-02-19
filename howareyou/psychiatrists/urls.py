f87ff525b89c99d48c96ce3e53b96017d7a1be25
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-02-19 21:11:48 +0600

    completed test result poll

--- howareyou/psychiatrists/urls.py
+++ howareyou/psychiatrists/urls.py
@@ -4,4 +4,5 @@ app_name = 'psychiatrists'
 urlpatterns = [
 	path('psychiatrist_home', views.psychiatrist_home, name='psychiatrist_home'),
 	path('test_result_poll', views.test_result_poll, name='test_result_poll'),
+	path('test_result/<int:test_result_id>', views.test_result_varify, name='test_result_varify'),
 ]
--- howareyou/psychiatrists/views.py
+++ howareyou/psychiatrists/views.py
@@ -17,3 +17,9 @@ def test_result_poll(request):
 	context['my'] = Psychiatrist.objects.get(username=request.session['username'])
 	context['testresults'] = TestResult.objects.filter(verifier = None)
 	return render(request , 'psychiatrists/test_result_poll.html',context)
+
+def test_result_varify(request,test_result_id):
+	context={}
+	context['my'] = Psychiatrist.objects.get(username=request.session['username'])
+	context['testresult'] = TestResult.objects.get(id=test_result_id)
+	return render(request, 'psychiatrists/test_result_varify.html',context)
